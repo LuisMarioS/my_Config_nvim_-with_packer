990501d5558bc69408509d2e5993c994d282bcf1
LuisMarioS <luismariosandoval103@gmail.com>
2023-02-19 11:24:15 -0400

    v0.3: Añadi el clickboard

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,4 +1,5 @@
 local opt = vim.opt
+local g = vim.g
 
 opt.number = true
 opt.shiftwidth = 4
@@ -9,3 +10,4 @@ opt.termguicolors = true
 --[[ opt.cursorline = true ]]
 opt.updatetime = 0
 opt.history = 0
+g.clickboard = 'unnamedplus'
